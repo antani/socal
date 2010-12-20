a121cd1f769a004fb24450b6ededa5760b302fd2
Ved <ved.antani@gmail.com>
2010-12-20 17:58:35 +0530

    Modified Twitter production API keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 require 'openid/store/filesystem'
-Rails.application.config.middleware.use OmniAuth::Builder do  
-   provider :twitter, 'vm1CDPRNqXHXseMnUKHxDA', 'O08Pt86u7n8mNhWdT78ODCAxm8UJjEJEyOkF6rPho'  
-   provider :facebook, '146622722052799', '60082246962cc913f932e2ed2854c30d' 
+Rails.application.config.middleware.use OmniAuth::Builder do
+   #Development key
+   #provider :twitter, 'vm1CDPRNqXHXseMnUKHxDA', 'O08Pt86u7n8mNhWdT78ODCAxm8UJjEJEyOkF6rPho'
+   provider :facebook, '146622722052799', '60082246962cc913f932e2ed2854c30d'
    provider :open_id, OpenID::Store::Filesystem.new('/tmp')
-end 
+   #Production
+   provider :twitter, 'LkNRuATCSidM7rF63NnUYw', '96cdM22QKD5ZeGuI2jv7wYXm7SuT1lBdj2hxWD1kwgA'
+
+end
+
