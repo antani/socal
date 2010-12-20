a0176612a264dc1bebce60d0711a6213d4a90859
Ved <ved.antani@gmail.com>
2010-12-20 21:40:14 +0530

    Modified mailer information host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Socal::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'http://morning-autumn-487.heroku.com' }
 end
+
