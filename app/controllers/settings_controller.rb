722c305804158889310fdb392a5dcd80ea553f3d
antani <antani@antani.(none)>
2011-01-20 19:14:18 +0530

    removed yahoo omni auth

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,101 +8,100 @@ class SettingsController < ApplicationController
   def create
     logger.debug 'Inisde settings.create'
     update
-
+    
   end
   # GET /calendars/1/edit
   def edit
     @user = current_user
   end
-
+  
   def update
     @user = current_user
     logger.debug 'Inside settings.update'
     logger.debug @user.email
     respond_to do |format|
-    
-          new_pwd = params[:user][:password]
-          new_cnf_pwd = params[:user][:password_confirmation]
-          curr_pwd = params[:user][:current_password]
-          
-          reminder = params[:user][:reminder_duration]
-          tz = params[:user][:timezone]
-          remind_before = params[:reminder_before_what]
-          if params[:share_twitter] != nil
-            st = true
-          else
-            st =false
-          end
-          if params[:share_facebook] != nil
-            sf = true
-          else
-            sf =false
-          end
-          if params[:share_foursquare] != nil
-            sff = true
-          else
-            sff =false
-          end
-
+      
+      new_pwd = params[:user][:password]
+      new_cnf_pwd = params[:user][:password_confirmation]
+      curr_pwd = params[:user][:current_password]
+      
+      reminder = params[:user][:reminder_duration]
+      tz = params[:user][:timezone]
+      remind_before = params[:reminder_before_what]
+      if params[:share_twitter] != nil
+        st = true
+      else
+        st =false
+      end
+      if params[:share_facebook] != nil
+        sf = true
+      else
+        sf =false
+      end
+      if params[:share_foursquare] != nil
+        sff = true
+      else
+        sff =false
+      end
+      
+      
+      #        logger.debug "current password ---------------------"
+      #        logger.debug curr_pwd
+      #        logger.debug "new password ---------------------"
+      #        logger.debug new_pwd
+      #        logger.debug "new confirm password ---------------------"
+      #        logger.debug new_cnf_pwd
+      if params[:user][:which_form] =='password'                                     
         
-#        logger.debug "current password ---------------------"
-#        logger.debug curr_pwd
-#        logger.debug "new password ---------------------"
-#        logger.debug new_pwd
-#        logger.debug "new confirm password ---------------------"
-#        logger.debug new_cnf_pwd
-        if params[:user][:which_form] =='password'                                              #1
+        if new_pwd.blank? or new_cnf_pwd.blank?                                     
+          flash[:error] = "New password or confirmation password is blank" 
+          format.html { redirect_to settings_home_url }        
+        end                                                                         
+        status= @user.update_with_password(:current_password=>curr_pwd,:password=>new_pwd,:password_confirmation =>new_cnf_pwd,:email => @user.email)                
+        logger.debug 'Status------------------'
+        logger.debug status
         
-        		if new_pwd.blank? or new_cnf_pwd.blank?                                      #2
-            	flash[:error] = "New password or confirmation password is blank" 
-               format.html { redirect_to settings_home_url }        
-        		end                                                                          #-2
-               status= @user.update_with_password(:current_password=>curr_pwd,:password=>new_pwd,:password_confirmation =>new_cnf_pwd,:email => @user.email)                
-               logger.debug 'Status------------------'
-               logger.debug status
-               
-               if !status                                                                #3  
-                  format.html { 
-                        flash[:error] =  @user.errors.full_messages.to_sentence
-                        logger.debug @user.errors
-                        param = 'none'
-                        @user.errors.each do |key,val|
-                            param = "user_"+ key.to_s                           
-                        end
-                        redirect_to settings_home_url({:param_1 => param })
-                        }
-                        logger.debug @user.errors
-               elsif @user.save
-               	    flash[:success] = "Password Updated"
-                      format.html { redirect_to settings_home_url }               	 
-               end	                                                                     #-3
-                        
-       else    
-		            @user.reminder_duration = reminder
-        				@user.timezone = tz
-        				@user.share_twitter = st
-        				@user.share_facebook = sf
-        				@user.share_foursquare = sff
-        				@user.remind_before_what = remind_before
-  
-               if @user.save                                                              #4 
-                   logger.debug "Settings saved....................................................."
-                   flash[:success] = "Settings updated"
-                   format.html { redirect_to settings_home_url }
-                   format.js
-               else
-                   logger.debug "Settings failed....................................................."
-                   flash[:error] = "Settings can't be saved"
-                   format.html { redirect_to settings_home_url }
-                   format.js
-               end                                                                       #-4
-       end                                                                               #-1  
-     end  
+        if !status                                                                  
+          format.html { 
+            flash[:error] =  @user.errors.full_messages.to_sentence
+            logger.debug @user.errors
+            param = 'none'
+            @user.errors.each do |key,val|
+              param = "user_"+ key.to_s                           
+            end
+            redirect_to settings_home_url({:param_1 => param })
+          }
+          logger.debug @user.errors
+        elsif @user.save
+          flash[:success] = "Password Updated"
+          format.html { redirect_to settings_home_url }               	 
+        end	                                                                     
+        
+      else    
+        @user.reminder_duration = reminder
+        @user.timezone = tz
+        @user.share_twitter = st
+        @user.share_facebook = sf
+        @user.share_foursquare = sff
+        @user.remind_before_what = remind_before
+        
+        if @user.save                                                              
+          logger.debug "Settings saved....................................................."
+          flash[:success] = "Settings updated"
+          format.html { redirect_to settings_home_url }
+          format.js
+        else
+          logger.debug "Settings failed....................................................."
+          flash[:error] = "Settings can't be saved"
+          format.html { redirect_to settings_home_url }
+          format.js
+        end                                                                       
+      end                                                                           
+    end  
   end
   
   def update_other_settings
-  	logger.debug "Inside update_other_settings..................................................."
+    logger.debug "Inside update_other_settings..................................................."
   end     
   
 end
-
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
    #Production
    provider :twitter, 'LkNRuATCSidM7rF63NnUYw', '96cdM22QKD5ZeGuI2jv7wYXm7SuT1lBdj2hxWD1kwgA'
    provider :facebook, '181816611843055', 'eaf9afe8ad70a9626dfd488ec882c237',{:scope =>"publish_stream,user_likes,friends_likes,email,offline_access"}
- #  provider :foursquare, 'Q00C0YHNLKLGGKLK2ZLLWY2YDPWDTZ2DSYJ1G2BQ1NAAL542', 'HAOZ1JEHPHKLNHFXUG5ZUGQJM3BVJ10OAJC5QUOFGJCOHH0V'
    provider :open_id, nil
-     use OmniAuth::Strategies::OpenID, nil, :name => 'yahoo', :identifier => "https://me.yahoo.com"
-     use OmniAuth::Strategies::OpenID, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+     use OmniAuth::Strategies::OpenID, nil, :name => "yahoo", :identifier => "https://me.yahoo.com"
+     use OmniAuth::Strategies::OpenID, nil, :name => "google", :identifier => 'https://www.google.com/accounts/o8/id'
+   
+   provider :foursquare, 'X41NC314YUNA32IDY4TI0VHWYOWQEYX1IKPPYC0XAVHKYHGD', 'VYWPGJSQ5UOJQYR0FK2LF3SG3SEKR0WJ1OQ3E1IKK1NNA2KZ'
 
    #Development key
+
    # provider :twitter, 'vm1CDPRNqXHXseMnUKHxDA', 'O08Pt86u7n8mNhWdT78ODCAxm8UJjEJEyOkF6rPho'
    # provider :facebook, '139636279427129', '66c2c368cb6b472e5dcf71407353261e',{:scope =>"publish_stream,user_likes,friends_likes,email,offline_access"}
     #provider :open_id, OpenID::Store::Filesystem.new('/tmp')
@@ -19,8 +21,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
 
 
-   provider :foursquare, 'X41NC314YUNA32IDY4TI0VHWYOWQEYX1IKPPYC0XAVHKYHGD', 'VYWPGJSQ5UOJQYR0FK2LF3SG3SEKR0WJ1OQ3E1IKK1NNA2KZ'
-#   provider :gowalla, 'd0daf476bcad498e845aac0e57d384cf', 'cd26140f06d748abad0c7b6d87ee7fd2'
+   #   provider :gowalla, 'd0daf476bcad498e845aac0e57d384cf', 'cd26140f06d748abad0c7b6d87ee7fd2'
 #    provider :google_apps, OmniAuth::Strategies::GoogleApps, OpenID::Store::Filesystem.new('/tmp')
   #  provider :open_id, OpenID::Store::Filesystem.new('/tmp'), {:name => "google", :domain => "https://www.google.com/accounts/o8/id" }
   #  provider :open_id, OpenID::Store::Filesystem.new('/tmp'), {:name => "yahoo", :domain => "https://me.yahoo.com"}
